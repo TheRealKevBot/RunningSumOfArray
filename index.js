1cba146ee427b0a36bb84d7a02a32128dddc0c23
TheRealKevBot <Kglas001@gmail.com>
2021-06-23 15:23:29 -0600

    First

---
+++ index.js
@@ -0,0 +1,12 @@
+// Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+// Return the running sum of nums. 
+
+function runningSum(nums) {
+
+}
+
+console.log(runningSum([1,2,3,4]))
+console.log(runningSum([1,1,1,1,1]))
+console.log(runningSum([3,1,2,10,1]))
+console.log(runningSum([30,10,20,100,10]))
